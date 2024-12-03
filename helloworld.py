9b221a60157502587a6bfb4afb8d0ca26044721e
BadiaTLS <badiatuahman@gmail.com>
2024-12-03 14:06:14 +0700

    pesan pada helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,2 @@
 
-print("Hello world, automatic poll scm enabled")
+print("Hello world, automatic poll scm enabled perubahan dilakukan melalui local computer dan di publish ke github serta dilakukan building docker melalui jenkins")
